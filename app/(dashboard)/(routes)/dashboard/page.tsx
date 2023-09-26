a17c7d344231a78425253db925f0ef6292e7b7f3
Ren Li <renli@uw.edu>
2023-09-26 16:35:24 -0700

    Setting up

---
+++ app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button";
+
+const DashboardPage = () => {
+  return (
+    <Button variant="destructive" size="lg">
+      CLick me
+    </Button>
+  );
+};
+export default DashboardPage;
