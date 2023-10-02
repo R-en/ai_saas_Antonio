01d3a16a8b5b66e3e498a580524cfc8f572a2ae5
Ren Li <renli@uw.edu>
2023-10-02 16:15:11 -0700

    added missing files

---
+++ app/(dashboard)/(routes)/dashboard/layout.tsx
@@ -0,0 +1,16 @@
+import Navbar from "@/components/navbar";
+import Sidebar from "@/components/sidebar";
+
+const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="h-full reltaive">
+      <div className="hidden h-full md:flex md:w-72 md:flex-col md:fixed md:inset-y-0 z-[80] bg-gray-900">
+        <Sidebar />
+      </div>
+      <div className="md:pl-72">
+        <Navbar /> {children}
+      </div>
+    </div>
+  );
+};
+export default DashboardLayout;
---
+++ components/sidebar.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import Link from "next/link";
+import Image from "next/image";
+import { Montserrat } from "next/font/google";
+
+import { cn } from "@/lib/utils";
+import {
+  Code,
+  ImageIcon,
+  LayoutDashboard,
+  MessageSquare,
+  Music,
+  Settings,
+  VideoIcon,
+} from "lucide-react";
+
+const montserrat = Montserrat({
+  weight: "600",
+  subsets: ["latin"],
+});
+
+const routes = [
+  {
+    label: "Dashboard",
+    icon: LayoutDashboard,
+    href: "/dashboard",
+    color: "text-sky-500",
+  },
+  {
+    label: "Conversation",
+    icon: MessageSquare,
+    href: "/dashboard",
+    color: "text-violet-500",
+  },
+  {
+    label: "Image Generation",
+    icon: ImageIcon,
+    href: "/dashboard",
+    color: "text-pink-500",
+  },
+  {
+    label: "Video Generation",
+    icon: VideoIcon,
+    href: "/dashboard",
+    color: "text-orange-500",
+  },
+  {
+    label: "Music Generation",
+    icon: Music,
+    href: "/dashboard",
+    color: "text-emerald-500",
+  },
+  {
+    label: "Code Generation",
+    icon: Code,
+    href: "/dashboard",
+    color: "tex-green-500",
+  },
+  {
+    label: "Settings",
+    icon: Settings,
+    href: "/dashboard",
+  },
+];
+
+const Sidebar = () => {
+  return (
+    <div className="space-y-4 py-4 flex flex-col h-full bg-[#111827] text-white">
+      <div className="px-3 py-3 flex-1">
+        <Link href="/dashboard" className="flex items-center pl-3 mb-14">
+          <div className="relative w-8 h-8 mr-4">
+            <Image fill alt="Logo" src="/logo.png" />
+          </div>
+          {/*cn overwrites  tailwind content if they already exist*/}
+          <h1 className={cn("text-2xl font-bold", montserrat.className)}>
+            Genius
+          </h1>
+        </Link>
+        <div className="space-y-1">
+          {routes.map((route) => (
+            <Link
+              href={route.href}
+              key={route.href}
+              className="text-sm group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-white hover:bg-white/10 rounded-lg transition"
+            >
+              <div className="flex items-center flex-1">
+                <route.icon className={cn("h-5 w-5 mr-3", route.color)} />
+                {route.label}
+              </div>
+            </Link>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
